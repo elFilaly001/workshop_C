ebf05a5a9ddb0aca66620d0353cfefe1550a2afd
El Filaly <elfilalyabdeljalil@gmail.com>
2023-09-14 11:43:01 +0100

    partie 1 was created and challenge 1-8 put in partie 1

---
+++ PARTIE 1/challenge 8/test8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (){
+    int a;
+
+    printf("enter votre nombre: ");
+    scanf("%d", &a);
+
+    printf("votre nombre on octal %o \n" ,a);
+    printf("votre nombre on hexadicimal %x" ,a);
+}
